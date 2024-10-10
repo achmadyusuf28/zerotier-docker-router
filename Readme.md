9b42b0846b8570416c45d62d1dfdbfd2aa5f5516
Achmad Yusuf <yufsprops@hotmail.com>
2024-10-10 22:30:14 +0700

    fix iptables routing, change parameter name to Readme

--- Readme.md
+++ Readme.md
@@ -4,7 +4,8 @@ This is a docker image that runs a ZeroTier and acts as a router for other conta
 ## Usage
 ```
 docker build -t zerotier-debian:latest .
-docker run --network zerotier-jelly --name zerotier-jelly --privileged zerotier-debian:latest <zerotier-network-id>
+docker run --network <docker-network-name> --name <container-name> --privileged zerotier-debian:latest <zerotier-network-id>
 ```
 
 follow zerotier managed routes [here](https://docs.zerotier.com/route-between-phys-and-virt/#configure-the-zerotier-managed-route) 
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -22,19 +22,20 @@ fi
 # Fetch ZeroTier network interface name dynamically (usually starts with zt)
 ZT_INTERFACE=$(ip link | grep zt | awk -F: '{print $2}' | xargs)
 
-# Docker internal network (connected to "zerotier-jelly") interface (usually eth0)
+# Docker internal network interface (usually eth0)
 DOCKER_INTERFACE="eth0"
 
 # Check if ZeroTier interface exists before applying rules.
 if [ -n "$ZT_INTERFACE" ]; then
   echo "Setting up iptables rules for forwarding traffic between $DOCKER_INTERFACE and $ZT_INTERFACE"
 
-  # NAT traffic going out ZeroTier interface
-  iptables -t nat -A POSTROUTING -o $ZT_INTERFACE -j MASQUERADE
+  # NAT traffic going out Docker interface
+  iptables -t nat -A POSTROUTING -o $DOCKER_INTERFACE -j MASQUERADE
 
   # Allow forwarding traffic between the two networks (Docker <-> ZeroTier)
   iptables -A FORWARD -i $DOCKER_INTERFACE -o $ZT_INTERFACE -j ACCEPT
   iptables -A FORWARD -i $ZT_INTERFACE -o $DOCKER_INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+
 else
   echo "Error: ZeroTier network interface was not found."
   exit 1
